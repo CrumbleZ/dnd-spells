7ce82e9592ef02997c112e0bc3797096a91b9c3e
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2018-10-03 13:32:08 +0200

    Fetcher now fetches and caches the HTML data of spell, ignoring the irrelevant

--- python/options.py
+++ python/options.py
@@ -1,15 +1,31 @@
-from argparse import ArgumentParser
 import fetcher
+from argparse import ArgumentParser
+from sys import exit as sysexit
 
 
 def main():
     #Define script options
     parser = ArgumentParser()
     parser.add_argument("-l", "--update-list", action="store_true",
-                        help="Updates the list of known spells",)
+                        help="Updates the list of known spells")
+    parser.add_argument("-d", "--download", action="store_true",
+                        help="Updates the spell cache, without overwriting existing files.")
+    parser.add_argument("-D", "--download-overwrite", action="store_true",
+                        help="Updates the spell cache and overwrites existing files.")
 
-    #Execute script options
     args = parser.parse_args()
 
+    #Handle options conflict
+    if args.download_overwrite and args.download:
+        print("You can't specify both caching options")
+        sysexit(0)
+
+    #Execute script options
     if args.update_list:
         fetcher.update_spell_list()
+
+    if args.download:
+        fetcher.get_all_spells(overwrite_existing=False)
+
+    if args.download_overwrite:
+        fetcher.get_all_spells(overwrite_existing=True)
