21b64370694e7d5956b17a6a27f44bcda49c2b0e
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2018-10-09 14:16:35 +0200

    Removed sensitive data... again, because fuck you git

--- python/tokens.py
+++ python/tokens.py
@@ -1,4 +1,4 @@
 # In order to fetch spells from paid content, insert here the key of
 # your CobaltSession cookie.
 
-COBALT_SESSION = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..WUN2fsVigafXEROECzhrjA.jKZxZpn-IDFBkUkPx29BeaDvJIPIYaChokhw6vA6Sb_tmZt_zRTKMc9-iBYLGXw9.BVJZ2TNQc0JvRr8C5NleXA"
+COBALT_SESSION = ""
